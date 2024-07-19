f6cb02a2664908cfe9bfedccf8689736c9ecc381
quangbaond <quangbaorp@gmail.com>
2024-07-19 17:54:24 +0700

    develop

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            AdminSeeder::class,
-            SettingSeeder::class,
-            CategorySeeder::class,
+            // AdminSeeder::class,
+            // SettingSeeder::class,
+            // CategorySeeder::class,
             SettingTimeInvitesSeeder::class
         ]);
     }
