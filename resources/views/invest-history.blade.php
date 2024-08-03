5e512b8e0e1b12c364ea58b16cff5c611ebbbb8d
quangbaond <quangbaorp@gmail.com>
2024-08-03 21:47:40 +0700

    develop

--- resources/views/invest-history.blade.php
+++ resources/views/invest-history.blade.php
@@ -43,7 +43,7 @@
                 @else
                 <td style="width: 150px">
                     <p style="color: rgb(255, 166, 0);margin-bottom :0">{{ number_format($invest->amount) }} VND</p>
-                    <p style="font-size: 12px; color #ccc; margin-bottom :0">{{ $invest->type == 1 ? '(Hoàn tiền đầu tư)' : '(Tiền lời góp vốn)'}}</p>
+                    <p style="font-size: 12px; color #ccc; margin-bottom :0">(trừ tiền đầu tư)</p>
                 </td>
                 @endif
                 {{-- <td>
@@ -59,14 +59,14 @@
                 </td> --}}
                 <td>
                     @if($invest->status == 0)
-                    <span style="color: #000">{{ number_format($invest->balance) }}</span>
+                    <span style="color: #000">{{ number_format($invest->balance) }} VND</span>
                     @elseif($invest->status == 1)
-                    <span  style="color: #000">{{ number_format($invest->balance) }}</span>
+                    <span  style="color: #000">{{ number_format($invest->balance) }} VND</span>
                     @elseif($invest->status == 2)
-                    <span  style="color: #000">{{ number_format($invest->balance + ($invest->amount * $invest->product->profit_everyday / 100)) }}</span>
+                    <span  style="color: #000">{{ number_format($invest->balance + ($invest->amount * $invest->product->profit_everyday / 100)) }} VND</span>
                     @else
                     <span style="color: #000">{{
-                       number_format($invest->balance +  $invest->product->min_invest) }}</span>
+                       number_format($invest->balance +  $invest->product->min_invest) }} VND</span>
                     @endif
                 </td>
                 <td>{{ $invest->created_at }}</td>
